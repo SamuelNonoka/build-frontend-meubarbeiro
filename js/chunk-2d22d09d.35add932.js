f0f0ddc852374a15f872a06278852eaf2b65a1c5
Samuel <nonokapereira@gmail.com>
2020-09-29 23:17:18 -0300

    commit inicial

---
+++ js/chunk-2d22d09d.35add932.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d22d09d"],{f690:function(e,t,n){"use strict";n.r(t);var a=function(){var e=this,t=e.$createElement,n=e._self._c||t;return n("site-template",{attrs:{menuButton:!0}},[n("p",[e._v("Alterar senha")])])},s=[],l=n("099b"),r={name:"ChangePassword",components:{SiteTemplate:l["a"]}},u=r,o=n("2877"),c=Object(o["a"])(u,a,s,!1,null,null,null);t["default"]=c.exports}}]);
+//# sourceMappingURL=chunk-2d22d09d.35add932.js.map
